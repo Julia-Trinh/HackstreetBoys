5d4c59fc5f0f224468fcc236e3a55288b4da7b9d
HoangVuLuu <hoangvuluu1624@gmail.com>
2025-02-01 14:48:50 -0500

    took out color theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import '@mantine/core/styles.css';
-import { createTheme } from '@mantine/core'
 import Home from "./pages/MainPage";
 
 const router = createBrowserRouter([
@@ -15,24 +13,6 @@ export default function App() {
           <RouterProvider router={router}></RouterProvider>
       </GlobalStateProvider>
 
-const theme = createTheme({
-  colors: {
-    'hsb-orange': [
-      '#ccd5ae',
-      '#b8c09d',
-      '#a3aa8b',
-      '#8f957a',
-      '#7a8068',
-      '#666b57',
-      '#525546',
-      '#3d4034',
-      '#292b23',
-      '#141511',
-    ],
-  },
-  primaryColor: 'hsb-orange',
-  primaryShade: 1,
-});
 
   );
 }
