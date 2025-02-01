99f6d1e01d8bee46faaeae4544698facebce1b57
Julia Trinh <juliatrinh2002@gmail.com>
2025-02-01 17:17:55 -0500

    navbar css

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,19 +1,19 @@
-import { Outlet } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
+import './NavBar.css';
 
 const NavBar = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-    return (
-        <div>
-            <div>
-                <button onClick={() => navigate("/home")}>HOME</button>
-                <button>PAGE 1</button>
-                <button>PAGE 2</button>
-            </div>
-        </div>
-    )
+  return (
+    <div className="navbar-container">
+      <div className="navbar-buttons">
+        <button className="nav-button" onClick={() => navigate("/home")}>HOME</button>
+        <button className="nav-button" onClick={() => navigate("/game")}>GAME</button>
+        <button className="nav-button" onClick={() => navigate("/leaderboard")}>LEADERBOARD</button>
+      </div>
+      <Outlet />
+    </div>
+  );
 };
 
 export default NavBar;
