97e3bc2be23f1106653ffe0f1abca0cb72422db5
Julia Trinh <juliatrinh2002@gmail.com>
2025-02-01 14:22:59 -0500

    added theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import '@mantine/core/styles.css';
+import { createTheme } from '@mantine/core'
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const theme = createTheme({
+  colors: {
+    'hsb-orange': [
+      '#ccd5ae',
+      '#b8c09d',
+      '#a3aa8b',
+      '#8f957a',
+      '#7a8068',
+      '#666b57',
+      '#525546',
+      '#3d4034',
+      '#292b23',
+      '#141511',
+    ],
+  },
+  primaryColor: 'hsb-orange',
+  primaryShade: 1,
+});
 
 export default App
