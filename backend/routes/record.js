6c5cf77be1a25e7fa349d97b9f33cfe4122f299f
Julia Trinh <juliatrinh2002@gmail.com>
2025-02-01 16:32:32 -0500

    header

--- backend/routes/record.js
+++ backend/routes/record.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const Record = require('../models/record');
 
-// GET TOP 5 RECORDS WITH SHORTEST timeToCompletion
+// GET TOP 5 RECORDS WITH longest timeToCompletion
 router.get('/get_top_5_records', async (req, res) => {
     try {
         // Fetch records sorted by timeToCompletion in ascending order, and limit to 5
         const records = await Record.find()
-            .sort({ timeToCompletion: 1 }) // Sort by timeToCompletion in ascending order
+            .sort({ timeToCompletion: -1 }) // Sort by timeToCompletion in ascending order
             .limit(5); // Limit to 5 records
 
         res.status(200).json(records);
--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "./MainPage.css";
-import Logo from "../components/SiteLogo.jsx";
 
 function MainPage() {
 
     return (
         <>
-            <Logo></Logo>
             <h1 className="siteName">Welcome to the Main Page</h1>
         </>
     )
